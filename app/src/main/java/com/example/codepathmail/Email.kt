df26d8ce1f24a2b948e800480f86641641132d43
Tayyab Nabeel <tayyab2390@gmail.com>
2025-09-17 11:59:11 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/codepathmail/Email.kt
@@ -0,0 +1,13 @@
+package com.example.codepathmail
+
+class Email (
+
+    val sender: String,
+    val title: String,
+    val summary: String) {
+
+}
+
+
+
+
---
+++ app/src/main/java/com/example/codepathmail/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.codepathmail
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var emails: List<Email>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val emailsRv = findViewById<RecyclerView>(R.id.emailsRV)
+        emails = EmailFetcher.getEmails()
+        val adapter = EmailAdapter(emails)
+        emailsRv.adapter = adapter
+
+        emailsRv.layoutManager = LinearLayoutManager(this)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
